22e3c096b0cb755729ab1beb5f934340a4bd305d
alium-official <alium@gmail.com>
2021-04-14 21:51:30 +0300

    init

--- src/styled.d.ts
+++
@@ -1,7 +0,0 @@
-import 'styled-components'
-import { AliumTheme } from './theme'
-
-declare module 'styled-components' {
-  /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface DefaultTheme extends AliumTheme {}
-}
